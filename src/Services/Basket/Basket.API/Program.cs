006e520fdca9fe0eea51148469e4cb6f64e46f3e
Philane Sigwebela <kaizer.pk@gmail.com>
2023-07-27 21:44:08 +0200

    Configured Redis and added entities

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -13,6 +13,11 @@ namespace Basket.API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
